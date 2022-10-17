4a2e1f91c16dd2108df3ce084f1c34c47e14c57c
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-10-17 17:36:42 +0200

    Add caching of API calls

--- drive/drive.py
+++ drive/drive.py
@@ -1,11 +1,10 @@
 import json
 import yaml
 import os
-import pathlib
-from service_environment import ServiceEnvironment
 
 CT = "json_data/"
 MANIFEST = "source_data/"
+API = "api_data/"
 
 class Drive():
     
@@ -13,28 +12,48 @@ class Drive():
     self.__scheme = scheme
     if scheme == "manifest":
       self.__dir = MANIFEST
+    elif scheme == "api":
+      self.__dir = API
     else:
       self.__dir = CT
 
   def read(self, filename):
     return self.read_from_cache(filename)
 
-  def present(self, filename):
-    list_result = self.__drive.list()
-    files = list_result.get("names")
-    if filename in files:
-      return True
-    return False
-
   def read_from_cache(self, filename):
     full_path = self.cache_full_path(filename)
-    with open(full_path) as infile:
-      if pathlib.Path(filename).suffix == ".yaml":
-        data = yaml.safe_load(infile)
+    with open(full_path) as in_file:
+      if self.extension(filename) == ".yaml":
+        data = yaml.safe_load(in_file)
       else:
-        data = json.load(infile)
+        data = json.load(in_file)
     return data
+
+  def write_to_cache(self, filename, data):
+    full_path = self.cache_full_path(filename)
+    with open(full_path, 'w') as out_file:
+      if self.extension(filename) == ".yaml":
+        yaml.dump(data, out_file, default_flow_style=False)
+      else:
+        json.dump(data, out_file)
+    return data
+
+  def dir_present(self, dir_name):
+    dir_path = self.cache_dir_path(dir_name)
+    if not os.path.exists(dir_path):
+      os.makedirs(dir_path)
+    return True
+
+  def file_present(self, filename):
+    full_path = self.cache_full_path(filename)
+    return os.path.exists(full_path)
 
   def cache_full_path(self, filename):
     return os.path.join(self.__dir, self.__scheme, filename)
     
+  def cache_dir_path(self, dir_name):
+    return os.path.join(self.__dir, self.__scheme, dir_name)
+
+  def extension(self, filename):
+    parts = os.path.splitext(filename)
+    return parts[1]
--- model/ct_api.py
+++ model/ct_api.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from drive.drive import Drive
 
 API_KEY = os.getenv('CDISC_API_KEY')
 BASE_URL = "https://api.library.cdisc.org/api/"
@@ -9,12 +10,23 @@ class CtApi():
   def __init__(self, package, date):
     self.__package = package
     self.__date = date
+    self.__drive = Drive("api")
   
   def read_code_lists(self):
-    return self.api_get("mdr/ct/packages/%sct-%s/codelists" % (self.__package, self.__date))
+    if self.cached_cls(self.__package, self.__date):
+      return self.read_cls(self.__package, self.__date)
+    else:
+      result = self.api_get("mdr/ct/packages/%sct-%s/codelists" % (self.__package, self.__date))
+      self.write_cls(self.__package, self.__date, result)
+      return result
 
   def read_code_list(self, identifier):
-    return self.api_get("mdr/ct/packages/%sct-%s/codelists/%s" % (self.__package, self.__date, identifier))
+    if self.cached_cl(self.__package, self.__date, identifier):
+      return self.read_cl(self.__package, self.__date, identifier)
+    else:
+      result = self.api_get("mdr/ct/packages/%sct-%s/codelists/%s" % (self.__package, self.__date, identifier))
+      self.write_cl(self.__package, self.__date, identifier, result)
+      return result
 
   def api_get(self, url):
     #print("CTAPI.API_GET [1]: %s" % (url))
@@ -24,3 +36,26 @@ class CtApi():
     response = requests.get(api_url, headers=headers)
     #print("CTAPI.API_GET [3]: %s %s" % (response.status_code, response.json()))
     return response.json()
+
+  def cached_cls(self, package, date):
+    return self.__drive.file_present("%s/%s/code_lists.json" % (package, date))
+  
+  def read_cls(self, package, date):
+    return self.__drive.read_from_cache("%s/%s/code_lists.json" % (package, date))
+
+  def write_cls(self, package, date, data):
+    self.__drive.dir_present("%s" % (package))
+    self.__drive.dir_present("%s/%s" % (package, date))
+    self.__drive.write_to_cache("%s/%s/code_lists.json" % (package, date), data)
+
+  def cached_cl(self, package, date, identifier):
+    return self.__drive.file_present("%s/%s/%s.json" % (package, date, identifier))
+  
+  def read_cl(self, package, date, identifier):
+    return self.__drive.read_from_cache("%s/%s/%s.json" % (package, date, identifier))
+  
+  def write_cl(self, package, date, identifier, data):
+    self.__drive.dir_present("%s" % (package))
+    self.__drive.dir_present("%s/%s" % (package, date))
+    self.__drive.write_to_cache("%s/%s/%s.json" % (package, date, identifier), data)
+
